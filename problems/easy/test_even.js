4c0ec318107425da7aba33d8710e2b17ea9e937f
renzosantamaria <renzo.santamaria@outlook.com>
2021-11-03 21:44:00 +0100

    edit test_even

--- problems/easy/test_even.js
+++ problems/easy/test_even.js
@@ -1,11 +1,14 @@
 import assert from 'assert'
 import { even } from './even.js'
 
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('Should return an array of even numbers', function() {
-            const result = even([1, 2, 3, 4, 5])
-            assert.deepEqual(result, [2, 4])
-        })
-    })
+describe('even', function () {
+  it('should return [2, 4] when [1,2,3,4,5] is passed as arg', function () {
+    assert.deepEqual(even([1, 2, 3, 4, 5]), [2, 4])
+  })
+  it('should return [] when [1,3,5] is passed as arg', function () {
+    assert.deepEqual(even([1, 3, 5]), [])
+  })
+  it('should return [-2] when [1,3,5,-2] is passed as arg', function () {
+    assert.deepEqual(even([1, 3, 5, -2]), [-2])
+  })
 })
