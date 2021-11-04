40406bae540b61ea2062181065655bfa608ee586
renzosantamaria <renzo.santamaria@outlook.com>
2021-11-04 18:01:22 +0100

    restore main branch

---
+++ problems/easy/even.js
@@ -0,0 +1,26 @@
+
+// Write a function that returns the even numbers in a given list.
+// eg. [1, 2, 3, 4, 5] -> [2, 4]
+
+export const even = (xs) => {
+	// Your code here...
+	let evenNumArray = []
+	xs.map( num => {
+		if (num % 2 === 0) {
+			evenNumArray.push(num)
+		}
+	})
+	return evenNumArray
+
+}
+
+const tests = [
+	[even, [[1, 2, 3, 4, 5]], [2, 4]],
+	[even, [[2, 4, 6]], [2, 4, 6]],
+	[even, [[1, 5, 9]], []],
+	[even, [[9, 13, 17, 99]], []],
+	[even, [[0, 2, 5, 182391, 283182372]], [0, 2, 283182372]],
+]
+
+export default tests
+
